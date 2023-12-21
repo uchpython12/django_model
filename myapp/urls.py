ff0d938a778872f83f4645588b3d733b189a4363
kai <uch.python12@gmail.com>
2023-12-21 18:02:33 +0800

    summary

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('other', views.other_view, name='other-view'),  # 注意路径中不带斜杠
+    path('', views.home, name='home'),
+    path('read/', views.read_data, name='read_data'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,9 +4,26 @@ from django.shortcuts import render
 
 from django.http import JsonResponse
 from django.http import HttpResponse
+
+from django.shortcuts import render
+from .models import YourModel
+
 def index(request):
     return JsonResponse({'message': 'Welcome to the homepage!'})
 
 def other_view(request):
     return HttpResponse("This is the other view!")
 
+def home(request):
+    return render(request, 'home.html')
+
+# views.py
+
+
+
+def read_data(request):
+    data = YourModel.objects.all()
+    # 创建一个新的 YourModel 实例
+    # new_instance = YourModel(name='John', age=30, email='john@example.com')
+    # new_instance.save()  # 保存实例到数据库中
+    return render(request, 'read_data.html', {'data': data})
